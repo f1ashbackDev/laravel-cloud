75fe5587152c5117629bce089ebf930aa8466027
f1ashbackDev <tapedappy@gmail.com>
2026-08-21 14:46:25 +0300

    Добавлен traits ApiResponser для ответов по api

---
+++ app/Http/Controllers/Api/CloudFilesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\CloudFiles;
+use Illuminate\Http\Request;
+
+class CloudFilesController extends Controller
+{
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(CloudFiles $cloudFiles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CloudFiles $cloudFiles)
+    {
+        //
+    }
+}
---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response as HttpResponse;
+
+trait ApiResponser {
+    private function sendResponse(?string $message=null, ?string $error = null, $data = null, bool $success = true, int $code = HttpResponse::HTTP_OK): JsonResponse 
+    {
+        return response()->json([
+            'success' => $success,
+            'message' => $message ?? $error,
+            'data' => $data
+        ], $code);
+    }
+
+    public function successResponse(?string $message=null, $data = null)
+    {
+        return $this->sendResponse(message: $message, data: $data);
+    }
+
+    public function errorResponse(?string $errorMessage=null, $data = null)
+    {
+        return $this->sendResponse(error: $errorMessage, data: $data, success: false, code: HttpResponse::HTTP_BAD_REQUEST);
+    }
+
+    public function notFoundResponse()
+    {
+        return $this->sendResponse("Запрашиваемый ресурс не найден!", success: false, code: HttpResponse::HTTP_NOT_FOUND);
+    }
+}
